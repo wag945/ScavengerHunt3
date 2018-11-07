dc3375c1a3f43a89a4cfe428da3a90392bddf9a8
williamganley <william.ganley@missionse.com>
2018-11-06 20:18:45 -0500

    Added Team Lobby

--- app/src/main/java/com/example/bill/scavengerhunt3/GameLobbyActivity.java
+++ app/src/main/java/com/example/bill/scavengerhunt3/GameLobbyActivity.java
@@ -1,13 +1,26 @@
 package com.example.bill.scavengerhunt3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameLobbyActivity extends AppCompatActivity {
+
+    private Button mTeamLobbyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_lobby);
+        mTeamLobbyButton = (Button) findViewById(R.id.teamLobbyButton);
+        mTeamLobbyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(GameLobbyActivity.this, TeamLobbyActivity.class);
+                GameLobbyActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
