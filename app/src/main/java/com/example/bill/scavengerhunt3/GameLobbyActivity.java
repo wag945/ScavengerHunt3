eca2b50cd02ba7faa90d476980b8c0fc495da9cc
williamganley <william.ganley@missionse.com>
2018-11-06 20:00:50 -0500

    Create skeleton of two activities

---
+++ app/src/main/java/com/example/bill/scavengerhunt3/GameLobbyActivity.java
@@ -0,0 +1,13 @@
+package com.example.bill.scavengerhunt3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameLobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_lobby);
+    }
+}
--- app/src/main/java/com/example/bill/scavengerhunt3/MainActivity.java
+++ app/src/main/java/com/example/bill/scavengerhunt3/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.bill.scavengerhunt3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, GameLobbyActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
